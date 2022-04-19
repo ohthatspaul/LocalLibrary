76153735e4788ddf734d81496919af526415be8b
talentmavingire <mavingiretalent@gmail.com>
2022-04-19 12:21:50 +0800

    defined the Genre model for the books

---
+++ models/genre.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema()
+
+const GenreSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+})
+
+// Virtual for Genre's url
+GenreSchema.virtual('url').get(function () {
+  return 'catalog/genre/' + this._id
+})
