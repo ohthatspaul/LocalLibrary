430ae76272a4c4ecc4973fa57526d229ed776ac1
talentmavingire <mavingiretalent@gmail.com>
2022-04-19 12:32:17 +0800

    fix typo on Schema declaration

--- models/genre.js
+++ models/genre.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema()
+const Schema = mongoose.Schema
 
 const GenreSchema = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 100 },
