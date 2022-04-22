d5e8bc3bd3e417bde18555f089174dc984260d1c
talentmavingire <mavingiretalent@gmail.com>
2022-04-22 18:08:34 +0800

    display the list of all books and count for the books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,14 +1,52 @@
 const Book = require('../models/book')
+const Author = require('../models/author')
+const Genre = require('../models/genre')
+const BookInstance = require('../models/bookInstance')
 
+const async = require('async')
 module.exports = {
   // Home page
   index: function (req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page')
+    async.parallel(
+      {
+        book_count: function (callback) {
+          Book.countDocuments({}, callback) // Pass an empty object as match condition to find all documents of this collection
+        },
+        book_instance_count: function (callback) {
+          BookInstance.countDocuments({}, callback)
+        },
+        book_instance_available_count: function (callback) {
+          BookInstance.countDocuments({ status: 'Available' }, callback)
+        },
+        author_count: function (callback) {
+          Author.countDocuments({}, callback)
+        },
+        genre_count: function (callback) {
+          Genre.countDocuments({}, callback)
+        },
+      },
+      function (err, results) {
+        res.render('index', {
+          title: 'Local Library Home',
+          error: err,
+          data: results,
+        })
+      }
+    )
   },
 
   // Display list of all books.
-  book_list: function (req, res) {
-    res.send('NOT IMPLEMENTED: Book list')
+  book_list: function (req, res, next) {
+    Book.find({}, 'title author')
+      .sort({ title: 1 })
+      .populate('author')
+      .exec(function (err, list_books) {
+        if (err) {
+          return next(err)
+        }
+        //Successful, so render
+        res.render('book_list', { title: 'Book List', book_list: list_books })
+      })
   },
 
   // Display detail page for a specific book.
