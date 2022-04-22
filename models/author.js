6e6d32390b94319e61919e6788dcd36689bce025
talentmavingire <mavingiretalent@gmail.com>
2022-04-22 17:57:20 +0800

    added virtuals for date formatting

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
 
 const Schema = mongoose.Schema
 
@@ -40,6 +41,18 @@ AuthorSchema.virtual('lifespan').get(function () {
 AuthorSchema.virtual('url').get(function () {
   return '/catalog/author/' + this._id
 })
+// Format the date of birth
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : ''
+})
 
+// Format the date of birth
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : ''
+})
 // Export model
 module.exports = mongoose.model('Author', AuthorSchema)
