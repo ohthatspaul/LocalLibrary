ef683579bc83c4abfd998409e3b1cc1d69467576
talentmavingire <mavingiretalent@gmail.com>
2022-04-22 17:58:16 +0800

    Display list of all BookInstances

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,10 +1,22 @@
 const res = require('express/lib/response')
 const BookInstance = require('../models/bookInstance')
+const { DateTime } = require('luxon')
 
 module.exports = {
   // Display list of all BookInstances
-  book_instance_list: function (req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list')
+  book_instance_list: function (req, res, next) {
+    BookInstance.find()
+      .populate('book')
+      .exec(function (err, list_book_instances) {
+        if (err) {
+          return next(err)
+        }
+        // Successful, so render
+        res.render('book_instance_list', {
+          title: 'Book Instance List',
+          book_instance_list: list_book_instances,
+        })
+      })
   },
 
   // Display detail page for a specific BookInstance
